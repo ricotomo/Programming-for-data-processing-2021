23fec442eb28afd04d31036edf71223bb1c4313f
Moritz Walther <friedicecream1@gmail.com>
2021-02-14 21:50:01 +0100

    Did the 5th exercise

--- Module-1-Exercises.py
+++ Module-1-Exercises.py
@@ -46,9 +46,24 @@ vals.sort()
 
 print("sum is: " + str(sum) + " average is: " + str(avg) + " lowest number is: " + str(vals[0]) + " highest number is: " + str(vals[len(vals)-1]) )
 
-#Write a program that asks the user to enter two words. Theprogram then prints out both words on one line, the lengthof both words and number of 
+#Write a program that asks the user to enter two words. The program then prints out both words on one line, the lengthof both words and number of 
 # points added. The words will be separated by enought dots so that the total line length is 30:F  i  r  s  t   w  o  r  d   :    t  u  r  t  l  eS  e  c  o  n  d                    w  o  r  d   :                   1  5  3t  u  r  t  l  e     .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   1   5   3L  e  n  g  t  h                     f  i  r  s  t                    w  o  r  d   :                   6L  e  n  g  t  h                     f  i  r  s  t                    w  o  r  d   :                   3N  u  m  b  e  r                    o  f                    p  o  i  n  t  s    :                                   2  1
+first = input("enter your first word ")
+second = input("enter your second word ")
+numdots = 30 - (len(first) +len(second))
+dots = ""
 
+for x in range(numdots):
+    dots = dots + "."
+
+print("The first word is: " + first)
+print("The second word is: " + second)
+print(first + dots + second)
+
+
+print("The length of the first word is: " + str(len(first)))
+print("The length of the second word is: " + str(len(second)))
+print("The number of dots is: " + str(len(dots)))
 
 #You are given the following DNA sequence:
 # A   A   A   C   G   C   T   G   T   C   A   A   T   A   C   A   A   T   C   T   T   Y   C   T   A   G   A   T   A   T   T   C   G   G   A   T   T   T   G   A   AT   T   T   T   G   C   A   A   A   A   A   G   T   C   C   G   A   A   G   C   T   G   C   C   C   A   C   C   T   C   A   A   G   T   C   A   T   T   G   T   TT   C   A   A   C   T   C   G   C   T   T   A   C   G   G   T   A   T   A   T   A   T   A   T   C   T   A   C   T   T   T   C   A   T   T   G   A   G   A   T   AT   A   A   A   C   A   G   C   G   C   T   G   A   T   A   C   A   A   T   C   T   T   T   T   T   A   T   A   T   A   A   G   T   C   T   T   T   T   G   T   AC   A   A   A   T   A   A   A   G   C   T   A   G   G   A   A   A   A   G   C   C   C   G   A   C   G   T   C   A   T   T   A   T   A   G   C   T   A   TAn imaginary restriction enzyme cuts in "AAGTCA" findwhere in the previous sequence this enzyme will cut
