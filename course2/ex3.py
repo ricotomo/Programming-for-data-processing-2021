976f7c9052ac91f583261e3005fec17134e1814c
Moritz Walther <friedicecream1@gmail.com>
2021-02-17 18:55:35 +0100

    added readsentance() to ex2

--- course2/ex3.py
+++ course2/ex3.py
@@ -1,1 +1,28 @@
-Module-2-Python-Data-Structures-Bilingual-Lexicon.py
+bilingual = {
+  "merry": "feliz",
+  "christmas": "navidad",
+  "and": "y",
+  "happy" : "prospero",
+  "new" : "nuevo",
+  "year" : "ano"
+}
+
+
+
+def readSentence():
+    engstr = input("Enter your English sentance: ")
+    print(engstr)
+    englist = str.split(engstr)
+    print(englist)
+    spanishstr = ""
+
+    for x in englist:
+        print(x)
+        try: 
+            spanishstr = spanishstr + bilingual[x] + " "
+        except:
+            "We're sorry a word you used isnt in our dictionary"
+            break
+    return spanishstr
+
+print(readSentence())
