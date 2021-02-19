b0857318ba8b716c03c10f05e0b0983b9b2c1fc2
Moritz Walther <friedicecream1@gmail.com>
2021-02-19 19:26:11 +0100

    Update instantiate.py

--- course3/instantiate.py
+++ course3/instantiate.py
@@ -1,3 +1,9 @@
 #do I need this import statement when theyre in the same directory? 
-import course3.cinema
+from course3 import cinema
 
+myFilm = cinema.Film("Star Wars: Revenge of the Sith", 2, 13, "George Lucas", 10.75)
+
+Mo = cinema.Spectator(1, 23, 20)
+Lena = cinema.Spectator(2, 23, 20)
+Tim = cinema.Spectator(3, 23, 20)
+Soo = cinema.Spectator(2, 23, 20)
