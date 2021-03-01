97585e563a169de38a7573fc580de7ee98f4f5b0
Moritz Walther <friedicecream1@gmail.com>
2021-03-01 12:10:54 +0100

    added searchArticles()
    Search articles returns the number of articles published

--- course5/scraper_regex.py
+++ course5/scraper_regex.py
@@ -14,4 +14,18 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 file = codecs.open((os.path.join(__location__, './Solomon_Kullback.html')), "r", "utf-8")
 text = file.read()
 
-print(text[:10])
+# The number of article published.
+#class="entry article toc"
+#itemtype="http://schema.org/ScholarlyArticle"
+#  len(re. findall(pattern, string))
+
+#function which returns the number of article published.
+def searchArticles(text):
+    pattern = re.compile(r'itemtype="http://schema.org/ScholarlyArticle"', re.IGNORECASE)
+    result = len(re.findall(pattern, text))
+    return result
+
+print("The number of articles published is " + str(searchArticles(text)))
+# The number of proceedings published.
+# The list of ids of the articles.The list of ids of the proceedings.
+# The name of the publications.IThe authors of each publications
